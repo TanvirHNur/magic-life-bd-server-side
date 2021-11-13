8f65c693b9945d33985c772c4fb4b669945b492d
TanvirHNur <tanvir.du.ier@gmail.com>
2021-11-13 06:23:30 +0600

    added make admin

--- index.js
+++ index.js
@@ -147,17 +147,22 @@ async function run(){
           });
 
 
-          app.put('/users/admin', async (req,res) => {
-            const user = req.body;
-            const filter = {email: user.email};
-            const updateDoc = {$set: 
-            user,};
-            const options = { upsert: true };
-            const result = await usersCollection.updateOne(filter, updateDoc,options);
-            console.log(user, result)
-            res.json(result);
-        });
+        //   app.put('/users/admin', async (req,res) => {
+        //     const user = req.body;
+        //     const filter = {email: user.email};
+        //     const updateDoc = {$set: {role: 'admin'}};
+        //     const result = await usersCollection.updateOne(filter, updateDoc);
+        //     console.log(user, result)
+        //     res.json(result);
+        // });
 
+        app.put('/users/admin',  async (req,res) => {
+          const user = req.body;
+          const filter = {email: user.email};
+          const updateDoc = {$set: {role: 'admin'}};
+          const result = await usersCollection.updateOne(filter, updateDoc);
+          res.json(result);
+      });
         
         
     }
